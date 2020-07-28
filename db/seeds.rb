2a3010ecfc4dbc4f0083c0aab9ea4efc441952d2
Lucien Lopez <lucien.lopez.ext@orange.com>
2020-07-28 21:21:46 +0200

    Created seed task

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+recipes = JSON.parse(File.read("#{Rails.root}/db/seeds/recipes.json"))
+recipes.each do |recipe_hash|
+  tags = recipe_hash['tags'].map { |tag| Tag.find_or_create_by(name: tag) }
+  ingredients = recipe_hash['ingredients'].map do |ingredient|
+    Ingredient.new(description: ingredient)
+  end
+  recipe_hash['tags'] = tags
+  recipe_hash['ingredients'] = ingredients
+  Recipe.create(recipe_hash.except('nb_comments'))
+end
