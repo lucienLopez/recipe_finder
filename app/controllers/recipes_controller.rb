b31a647ae76d61d1d61264d6b2353073a0d9356e
Lucien Lopez <lucien.lopez.ext@orange.com>
2020-07-29 23:18:00 +0200

    Recipe controller: better empty method declaration

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,13 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: :show
 
-  def index
-  end
+  def index; end
 
   def search
     render json: Recipe.find_by_ingredients(params[:search_text]).limit(30)
   end
 
-  def show
-  end
+  def show; end
 
   private
 
